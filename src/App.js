7a73d3b2ccdf08aa2d77590fadc3388e93331a59
Madalina Teaca <madalina1teaca@gmail.com>
2023-07-05 19:35:39 +0300

    02. Setting up light/dark mode themes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import { Calendar } from "@fullcalendar/core";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      {/* ⬆ setting up the colors for the app */}
+
+      {/* ⬇ setting up light/dark mode for the app*/}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+          </main>
+        </div>
+      </ThemeProvider>
+      
+    </ColorModeContext.Provider>
   );
 }
 
